d67175baea65d64199c3ed3e915bc538ce52a427
efortin <fortin.emmanuel@gmail.com>
2026-05-05 18:35:42 +0200

    chore(release): update Alfred cask to 0.1.13

--- Casks/alfred-app.rb
+++ Casks/alfred-app.rb
@@ -1,6 +1,6 @@
 cask "alfred-app" do
-  version "0.1.12"
-  sha256 "eeac832fa6b1609ed0fc746c85804600f9c36cf060fdd7000dc16946dfd60d31"
+  version "0.1.13"
+  sha256 "8458bca37f1852fee9e7f81376bbbaff3607b122a2fe48d7fba1c15e4c466d98"
   url "https://github.com/alfred-labs/alfred-releases/releases/download/v#{version}/Alfred-#{version}-darwin-arm64.dmg",
       verified: "github.com/alfred-labs/alfred-releases/"
   name "Alfred"
