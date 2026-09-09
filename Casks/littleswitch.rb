3ad8dc82f9ef9791799a5c4bc11d18634ffe04f6
efortin <efortin@users.noreply.github.com>
2026-09-09 16:02:04 +0200

    release: LittleSwitch 0.4.2

--- Casks/littleswitch.rb
+++ Casks/littleswitch.rb
@@ -1,6 +1,6 @@
 cask "littleswitch" do
-  version "0.4.1"
-  sha256 "cd983ee5f784cd6d47698564c936ecce1c4fcd37e6a1f8a9e98c6d50ad39663d"
+  version "0.4.2"
+  sha256 "17645ac7875645bf0269fcadcc52f41c6efe790ef9d003564767565b64601d7b"
 
   url "https://github.com/alfred-labs/little-switch-releases/releases/download/v#{version}/LittleSwitch-#{version}-arm64.dmg"
   name "LittleSwitch"
